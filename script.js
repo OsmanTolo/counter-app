435f39b4f1154cbfc432ec0a3f2840d4112943d3
osmanTolo <dumbuyaosman@icloud.com>
2022-09-29 18:12:31 +0100

    Add counter logic

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-"use strict";
-
-const increase = document.getElementById("increase");
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
+const outputValue = document.getElementById("output");
+const increaseValue = document.getElementById("increase");
+const decreaseValue = document.getElementById("decrease");
+const resetValue = document.getElementById("reset");
 
 let counter = 0;
+
+const incrementOutput = () => {
+  counter++;
+  outputValue.innerHTML = counter;
+};
+
+const decrementOutput = () => {
+  if (counter > 0) {
+    counter--;
+  } else {
+    resetOutput();
+  }
+  outputValue.innerHTML = counter;
+};
+
+const resetOutput = () => {
+  counter = 0;
+};
+
+increaseValue.addEventListener("click", incrementOutput);
+decreaseValue.addEventListener("click", decrementOutput);
