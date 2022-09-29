81dca3606b4e1e7d118439fad142bf2bfb07d24e
osmanTolo <dumbuyaosman@icloud.com>
2022-09-29 17:52:03 +0100

    Add initial state for JS

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+
+let counter = 0;
